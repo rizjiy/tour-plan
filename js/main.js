c338a466ce8031ee3fcf95eadc3a93f2c650487a
rizjiy <kirmenyov@mail.ru>
2020-11-30 23:17:18 +0300

    create: section reviews in index.html fix: hotel slider

--- js/main.js
+++ js/main.js
@@ -1,11 +1,20 @@
-var mySwiper = new Swiper('.swiper-container', {
-    // Optional parameters
-    
+var hotelSlider = new Swiper('.hotel-slider', {
     loop: true,
-    // Navigation arrows
     navigation: {
-      nextEl: '.slider-button--next',
-      prevEl: '.slider-button--prev',
+      nextEl: '.hotel-slider__button--next',
+      prevEl: '.hotel-slider__button--prev',
+    },
+    keyboard: {
+        enabled: true,
+        onlyInViewport: false,
+      },
+  })
+
+  var reviewsSlider = new Swiper('.reviews-slider', {
+    loop: true,
+    navigation: {
+      nextEl: '.reviews-slider__button--next',
+      prevEl: '.reviews-slider__button--prev',
     },
     keyboard: {
         enabled: true,
